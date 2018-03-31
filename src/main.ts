23b98c0d81b8548ccde45e6dec356f3d6c984429
pocketberserker <pocketberserker@gmail.com>
2018-03-31 17:01:51 +0900

    スナップショット

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,17 @@
 "use strict";
 import {engine} from "@cowlick/engine";
 
-function main() {
+module.exports = (param: g.GameMainParameterObject) => {
+  if (param.snapshot) {
+    require("snapshotLoader")(param.snapshot);
+  } else {
+    require("load")(engine);
+    const controller = engine.load("start");
 
-  require("load")(engine);
-  engine.load("start");
-}
-
-module.exports = main;
+    g.game.snapshotRequest.add(() => {
+      if (g.game.shouldSaveSnapshot()) {
+        g.game.saveSnapshot(controller.snapshot());
+      }
+    });
+  }
+};
