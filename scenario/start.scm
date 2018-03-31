9fd2be586217aad1e37fe9d2e07b6cc5c2ace54e
pocketberserker <pocketberserker@gmail.com>
2018-03-31 16:25:53 +0900

    座標修正

--- scenario/start.scm
+++ scenario/start.scm
@@ -2,30 +2,30 @@
   (frame
     (image "black" (layer (name "background") (x 0) (y 0)))
     (layer (name "message") (opacity 0))
-    (image "s1" (layer (name "base") (x 0) (y 0)))
+    (image "s1" (layer (name "base") (x 128) (y 0)))
   )
   (frame
-    (image "s2" (layer (name "base") (x 0) (y 0)))
+    (image "s2" (layer (name "base") (x 128) (y 0)))
     (text (clear #t) "")
     (layer (name "message") (opacity 0))
   )
   (frame
-    (image "s3" (layer (name "base") (x 0) (y 0)))
+    (image "s3" (layer (name "base") (x 128) (y 0)))
     (text (clear #t) "")
     (layer (name "message") (opacity 0))
   )
   (frame
-    (image "s4" (layer (name "base") (x 0) (y 0)))
+    (image "s4" (layer (name "base") (x 128) (y 0)))
     (text (clear #t) "")
     (layer (name "message") (opacity 0))
   )
   (frame
-    (image "s5" (layer (name "base") (x 0) (y 0)))
+    (image "s5" (layer (name "base") (x 128) (y 0)))
     (text (clear #t) "")
     (layer (name "message") (opacity 0))
   )
   (frame
-    (image "s6" (layer (name "base") (x 0) (y 0)))
+    (image "s6" (layer (name "base") (x 128) (y 0)))
     (text (clear #t) "")
     (layer (name "message") (opacity 0))
   )
